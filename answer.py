f739a9a5a2ff863a030779a4be0f69e1b3306697
bmaurer1-stevens <157402056+bmaurer1-stevens@users.noreply.github.com>
2024-01-22 12:34:03 -0500

    Update 1

--- answer.py
+++ answer.py
@@ -2,16 +2,21 @@ def hello_world():
     # Write a doc string below. Doc strings can be used to provide the user with a
     # description of what the function does.
     """
-
+    prints "Hello, World!"
     """
 
     # Write a comment; comments are very useful to other programmers.
 
     # Write a Python statement to print Hello, World!
+    print("Hello, World!")
 
 def add(a,b):
-
+    """
+    sums two variables
+    a: int
+    b: int
+    """
     # Write simple code to add variable a and b
-    output =
+    output = a + b
 
     return output
